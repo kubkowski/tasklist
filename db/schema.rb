06764ac620510b5d59cc35cb50402b5dd215bc54
kubkowski <jakub.opara@gmail.com>
2014-05-14 21:47:59 +0200

    Fixed header and task-user relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513152209) do
+ActiveRecord::Schema.define(version: 20140514193119) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20140513152209) do
     t.date     "planned_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",            null: false
